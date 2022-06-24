ecfbc8095d80fdca8ea92dd305e72f8c9d06d825
ezequiel.merecias <ezequiel.merecias@REM-LT-220601C>
2022-06-23 20:51:13 -0600

    Add project files.

---
+++ WizLib_DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizLib_Model.Models;
+
+namespace WizLib_DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
