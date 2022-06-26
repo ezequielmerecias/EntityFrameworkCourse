314ccb9fbb78a582a39e0ed66eb9fce3e5631f57
ezequiel.merecias <ezequiel.merecias@REM-LT-220601C>
2022-06-25 19:00:53 -0600

    Section 5

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WizLib_DataAccess;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+    }
+}
---
+++ WizLib_DataAccess/Migrations/20220625225439_addRawCategoryToTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizLib_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRawCategoryToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
